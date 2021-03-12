0a2561331a02aaa82fd7abe1e49acb6c0720276a
Ismael Ribeiro <coragethedogcowardle@gmail.com>
2021-03-11 22:30:39 -0300

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -5,14 +5,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDK5dIpQHjYXeZ4ErWxsZu3TE611FeN8Fo",
-  authDomain: "alga-zap-c7aa4.firebaseapp.com",
-  databaseURL: "https://alga-zap-c7aa4.firebaseio.com",
-  projectId: "alga-zap-c7aa4",
-  storageBucket: "alga-zap-c7aa4.appspot.com",
-  messagingSenderId: "281051438793",
-  appId: "1:281051438793:web:6cf89ea664b557752d3718",
-  measurementId: "G-YDR89YJGT5"
+  apiKey: "AIzaSyAWAKBgbDh4JoGPFU5JXFfs9hiI-qTTFMc",
+  authDomain: "unoo-307018.firebaseapp.com",
+  projectId: "unoo-307018",
+  storageBucket: "unoo-307018.appspot.com",
+  messagingSenderId: "782781320153",
+  appId: "1:782781320153:web:ddbcfa702642396c3827c8",
+  measurementId: "G-W1BLNWMFN6"
 })
 
 export { firebase }
